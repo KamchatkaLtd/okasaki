5c48504803716b4c01e3629c7be86d33fa26c8a6
Alexey Filippov <afilippov@acm.org>
2015-06-30 07:31:14 +0100

    Ex. 3.9 Recursive descent with a red budget Ex. 3.10a lbalance/rbalance split

--- src/main/scala/okasaki/RedBlackSet.scala
+++ src/main/scala/okasaki/RedBlackSet.scala
@@ -1,5 +1,8 @@
 package okasaki
 
+import java.lang.Math.max
+
+import okasaki.Aux.log2
 import okasaki.RedBlackSet.RBTree
 
 /**
@@ -29,13 +32,28 @@ object RedBlackSet {
   }
 
   // ex. 3.9
-  def fromOrdList[E](l: List[E]): RBTree[E] = l match {
-    case Nil => Empty
-    case x :: Nil => SubTree(R, Empty, x, Empty) // ??? not good enough
-    case _ =>
-      val halves = l.splitAt(l.size / 2)
-      SubTree(B, fromOrdList(halves._1), halves._2.head, fromOrdList(halves._2))
+  def fromOrdList[E](l: List[E]): RBTree[E] = {
+    def build(l: List[E], rb: Int): RBTree[E] = (l, rb) match {
+      case (Nil, 0) => Empty
+      case (Nil, _) => throw new IllegalStateException(s"Red budget of $rb allocated for an empty list")
+      case (x :: Nil, 0) => SubTree(B, Empty, x, Empty)
+      case (x :: Nil, 1) => SubTree(R, Empty, x, Empty)
+      case (x :: Nil, _) => throw new IllegalStateException(s"Red budget of $rb allocated for a singleton list")
+      case (x :: y :: Nil, 1) => SubTree(B, SubTree(R, Empty, x, Empty), y, Empty)
+      case (x :: y :: Nil, _) => throw new IllegalStateException(s"Red budget of $rb allocated for a list of size 2")
+      case (x :: y :: z :: Nil, 0) => SubTree(B, SubTree(B, Empty, x, Empty), y, SubTree(B, Empty, z, Empty))
+      case (x :: y :: z :: Nil, 2) => SubTree(B, SubTree(R, Empty, x, Empty), y, SubTree(R, Empty, z, Empty))
+      case (x :: y :: z :: Nil, _) => throw new IllegalStateException(s"Red budget of $rb allocated for a list of size 3")
+      case _ =>
+        val halves = l.splitAt(l.size / 2)
+        SubTree(B, build(halves._1, (rb + 1) / 2), halves._2.head, build(halves._2.tail, rb / 2))
+    }
+
+    val redBudget = l.size - nearestFullTreeSmallerThan(l.size)
+    build(l, redBudget)
   }
+
+  def nearestFullTreeSmallerThan(n: Int): Int = (1 << log2(n + 1)) - 1
 }
 
 class RedBlackSet[E](implicit ord: Ordering[E]) extends Set[E, RBTree[E]] {
@@ -57,10 +75,25 @@ class RedBlackSet[E](implicit ord: Ordering[E]) extends Set[E, RBTree[E]] {
 
     member1(None, s)
   }
+  
+  def blackHeight(s: RBTree[_]): Int = s match {
+    case Empty => 0
+    case SubTree(R, a, _, b) => max(blackHeight(a), blackHeight(b))
+    case SubTree(B, a, _, b) => 1 + max(blackHeight(a), blackHeight(b))
+  }
 
-  def balance(clr: Color, l: RBTree[E], e: E, r: RBTree[E]) = (clr, l, e, r) match {
+  def isValid(s: RBTree[_]): Boolean = s match {
+    case Empty => true
+    case SubTree(_, a, _, b) => blackHeight(a) == blackHeight(b) && isValid(a) && isValid(b)
+  }
+
+  def lbalance(clr: Color, l: RBTree[E], e: E, r: RBTree[E]) = (clr, l, e, r) match {
     case (B, SubTree(R, SubTree(R, a, x, b), y, c), z, d) => SubTree(R, SubTree(B, a, x, b), y, SubTree(B, c, z, d))
     case (B, SubTree(R, a, x, SubTree(R, b, y, c)), z, d) => SubTree(R, SubTree(B, a, x, b), y, SubTree(B, c, z, d))
+    case _ => SubTree(clr, l, e, r)
+  }
+
+  def rbalance(clr: Color, l: RBTree[E], e: E, r: RBTree[E]) = (clr, l, e, r) match {
     case (B, a, x, SubTree(R, SubTree(R, b, y, c), z, d)) => SubTree(R, SubTree(B, a, x, b), y, SubTree(B, c, z, d))
     case (B, a, x, SubTree(R, b, y, SubTree(R, c, z, d))) => SubTree(R, SubTree(B, a, x, b), y, SubTree(B, c, z, d))
     case _ => SubTree(clr, l, e, r)
@@ -70,8 +103,8 @@ class RedBlackSet[E](implicit ord: Ordering[E]) extends Set[E, RBTree[E]] {
     def ins(ss: RBTree[E]): SubTree[E] = ss match {
       case Empty => SubTree(R, Empty, x, Empty)
       case sss@SubTree(color, a, y, b) =>
-        if (ord.lt(x, y)) balance(color, ins(a), y, b)
-        else if (ord.lt(y, x)) balance(color, a, y, ins(b))
+        if (ord.lt(x, y)) lbalance(color, ins(a), y, b)
+        else if (ord.lt(y, x)) rbalance(color, a, y, ins(b))
         else sss
     }
 
--- src/test/scala/okasaki/RedBlackSetSpec.scala
+++ src/test/scala/okasaki/RedBlackSetSpec.scala
@@ -7,8 +7,8 @@ import org.scalacheck.{Arbitrary, Gen}
  * Copyright (C) 2015 Kamchatka Ltd
  */
 
-class RedBlackSetSpec extends SetSpec[Int, RBTree[Int]]{
-  override def set: Set[Int, RBTree[Int]] = new RedBlackSet[Int]()
+class RedBlackSetSpec extends SetSpec[Int, RBTree[Int]] {
+  override def set: RedBlackSet[Int] = new RedBlackSet[Int]()
 
   implicit def elements: Arbitrary[Int] = Arbitrary(Gen.choose(Int.MinValue, Int.MaxValue))
 
@@ -19,10 +19,24 @@ class RedBlackSetSpec extends SetSpec[Int, RBTree[Int]]{
       a.forall(set.member(_, s)) should beTrue
     }
 
+    "be balanced" ! prop { (a: List[Int]) =>
+      val s = RedBlackSet.fromOrdList(a.sorted)
+
+      set.isValid(s) should beTrue
+    }
+
     "not contain extra elements" ! prop { (a: List[Int], e: Int) =>
       val s = RedBlackSet.fromOrdList(a.sorted)
 
       set.member(e, s) === a.contains(e)
+    }
+  }
+
+  "red-black set" should {
+    "be balanced" ! prop { (a: Seq[Int]) =>
+      val s = setFrom(a)
+
+      set.isValid(s) should beTrue
     }
   }
 }
