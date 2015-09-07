12a67b99ba54bcf25e94fc6611c6c2063575eb71
Alexey Filippov <afilippov@acm.org>
2015-09-07 07:49:34 +0100

    Ex. 9.3 Sparse representation

---
+++ src/test/scala/okasaki/SparseBinaryRandomAccessListSpec.scala
@@ -0,0 +1,41 @@
+package okasaki
+
+import okasaki.SparseBinaryRandomAccessList._
+import org.scalacheck.Arbitrary
+import org.scalacheck.Arbitrary._
+import org.scalacheck.Gen.choose
+
+/**
+ * Copyright (C) 2015 Kamchatka Ltd
+ */
+class SparseBinaryRandomAccessListSpec
+  extends RandomAccessListSpec[Int, SRList[Int], SparseBinaryRandomAccessList[Int]](new SparseBinaryRandomAccessList[Int])
+  with IntElements {
+
+  "Binary random access list" should {
+    "allow drop(n)" ! prop {
+      in: (List[Int], Int) => {
+        val (xs, n) = in
+        val l = fromList(xs)
+
+        drain(list.drop(n, l)) === xs.drop(n)
+      }
+    }
+  }
+
+  "repeat(n)" should {
+    "support any non-negative n" ! prop {
+      (a: Int, n: (List[Int], Int)) =>
+        val index = n._2
+        drain(repeat(index, a)) === List.iterate(a, index)(identity)
+    }
+  }
+
+
+  implicit def arrayWithIndex: Arbitrary[(List[Int], Int)] = Arbitrary(
+    for {
+      xs <- arbitrary[List[Int]]
+      n <- choose(0, xs.size)
+    } yield (xs, n)
+  )
+}
