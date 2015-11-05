a92e9bc1f39753045528043e44655bbe76d01181
Alexey Filippov <afilippov@acm.org>
2015-11-05 07:19:40 +0000

    Ex. 9.12 Segmented redundant binary numbers

---
+++ src/main/scala/okasaki/SegmentedRedundantBinaryNumbers.scala
@@ -0,0 +1,104 @@
+package okasaki
+
+/**
+ * Copyright (C) 2015 Kamchatka Ltd
+ */
+object SegmentedRedundantBinaryNumbers {
+
+  sealed trait Digit {
+    val value: Long
+    val multiplier: Long = 2L
+  }
+
+  case object Zero extends Digit {
+    val value = 0L
+  }
+
+  case class Ones(n: Int) extends Digit {
+    override val multiplier = 1L << n
+
+    val value = multiplier - 1
+  }
+
+  case object Two extends Digit {
+    val value = 2L
+  }
+
+  case class Threes(n: Int) extends Digit {
+    override val multiplier = 1L << n
+
+    val value = 3 * (multiplier - 1)
+  }
+
+  case object Four extends Digit {
+    val value = 4L
+  }
+
+  type Number = List[Digit]
+
+  def ones(n: Int, ds: Number): Number = (n, ds) match {
+    case (0, _) => ds
+    case (_, Ones(m) :: ds1) => Ones(n + m) :: ds1
+    case _ => Ones(n) :: ds
+  }
+
+  def threes(n: Int, ds: Number): Number = (n, ds) match {
+    case (0, _) => ds
+    case (_, Threes(m) :: ds1) => Threes(n + m) :: ds1
+    case _ => Threes(n) :: ds
+  }
+
+  def simpleInc(x: Number): Number = x match {
+    case Nil => ones(1, Nil)
+    case Zero :: ds => ones(1, ds)
+    case Ones(n) :: ds => Two :: ones(n - 1, ds)
+    case Two :: ds => threes(1, ds)
+    case Threes(n) :: ds => Four :: threes(n - 1, ds)
+    case Four :: ds => Zero :: simpleInc(ds)
+  }
+
+  def simpleDec(x: Number): Number = x match {
+    case Nil => throw new IllegalArgumentException("Cannot decrement zero")
+    case Zero :: ds => Four :: simpleDec(ds)
+    case Ones(n) :: ds => Zero :: ones(n - 1, ds)
+    case Two :: ds => ones(1, ds)
+    case Threes(n) :: ds => Two :: threes(n - 1, ds)
+    case Four :: ds => threes(1, ds)
+  }
+
+  def fixup(x: Number) = x match {
+    case Zero :: Nil => Nil
+    case Zero :: ds => Two :: simpleDec(ds)
+    case Ones(n) :: Zero :: ds => Ones(n) :: Zero :: simpleDec(ds)
+    case Threes(n) :: Four :: ds => Threes(n) :: Zero :: simpleInc(ds)
+    case Four :: ds => Two :: simpleInc(ds)
+    case _ => x
+  }
+
+  def inc(x: Number) = fixup(simpleInc(x))
+
+  def dec(x: Number) = fixup(simpleDec(x))
+
+  implicit class NumberOps(ds: Number) {
+    def asLong: Long =
+      ds.foldRight(0L)((d, a) => a * d.multiplier + d.value)
+  }
+
+
+  def toNumber(x: Long): Number = {
+    def twice(ds: Number): Number = ds match {
+      case Nil => Nil
+      case _ => Two :: dec(ds)
+    }
+
+    def toNumber(x: Long, ds: Number): Number = x match {
+      case _ if x < 0 => throw new IllegalArgumentException(s"toNumber($x)")
+      case 0 => Nil
+      case _ =>
+        val rounded = twice(toNumber(x / 2, ds))
+        if (x % 2 == 1) inc(rounded) else rounded
+    }
+
+    toNumber(x, Nil)
+  }
+}
---
+++ src/test/scala/okasaki/SegmentedRedundantBinaryNumbersSpec.scala
@@ -0,0 +1,70 @@
+package okasaki
+
+import okasaki.SegmentedRedundantBinaryNumbers._
+import org.specs2.ScalaCheck
+import org.specs2.mutable.Specification
+
+/**
+ * Copyright (C) 2015 Kamchatka Ltd
+ */
+class SegmentedRedundantBinaryNumbersSpec extends Specification with ScalaCheck {
+  "Segmented redundant binary numbers" should {
+    "have reasonable values" in {
+      List(Ones(8)).asLong === 255
+      List(Threes(2)).asLong === 9
+      List(Zero, Ones(1)).asLong === 2
+      List(Zero, Zero, Ones(1)).asLong === 4
+      List(Zero, Zero, Zero, Zero, Ones(1)).asLong === 16
+      List(Zero, Four).asLong === 8
+      List(Ones(2), Zero, Ones(1)).asLong === 11
+    }
+
+    "be easy to create" >> {
+
+      "simple examples" >> {
+        toNumber(0) === Nil
+        toNumber(1) === List(Ones(1))
+        toNumber(2) === List(Two)
+        toNumber(3) === List(Threes(1))
+        toNumber(4) === List(Two, Ones(1))
+        toNumber(9) === List(Threes(1), Ones(2))
+        toNumber(1023) === List(Threes(1), Two, Two, Two, Two, Two, Two, Two, Two)
+      }
+
+      "toNumber is correct" ! prop {
+        (x: Int) => (x >= 0) ==>
+          (toNumber(x).asLong === x)
+      }
+    }
+
+    "support increment" in {
+      "inc(a) == a + 1" ! prop { (a: Int) => (a >= 0) ==>
+        (inc(toNumber(a)).asLong === a + 1L)
+      }
+
+      "simple examples" >> {
+        inc(toNumber(0)) === List(Ones(1))
+        inc(toNumber(1)) === List(Two)
+        inc(toNumber(2)) === List(Threes(1))
+        inc(toNumber(3)) === List(Two, Ones(1))
+        inc(toNumber(4)) === List(Threes(1), Ones(1))
+      }
+    }
+
+    "support decrement" in {
+      "dec(a) == a + 1" ! prop { (a: Int) => (a > 0) ==>
+        (dec(toNumber(a)).asLong === a - 1L)
+      }
+
+      "simple examples" >> {
+        dec(toNumber(1)) === Nil
+        dec(toNumber(2)) === List(Ones(1))
+        dec(toNumber(3)) === List(Two)
+        dec(toNumber(4)) === List(Ones(2))
+        dec(toNumber(5)) === List(Two, Ones(1))
+        dec(toNumber(6)) === List(Ones(1), Two)
+      }
+    }
+  }
+
+}
